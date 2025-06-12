9d1f82a6b6594716877f7989e1508e8933e2fa54
Rijnder Wever <rien334@gmail.com>
2025-06-13 01:29:49 +0200

    tests: improve coverage

--- tests/test_custom_validate.py
+++ tests/test_custom_validate.py
@@ -19,6 +19,19 @@ def test_validate_recursive(shared_informatieobject):
     ):
         shared_informatieobject.validate()
 
+def test_validate_sequence_type_checking(shared_informatieobject):
+    """Test validation of sequence type checking."""
+
+    shared_informatieobject.identificatie = [shared_informatieobject.identificatie, VerwijzingGegevens("foo")]
+    with pytest.raises(ValidationError, match=r"\w+(\.\w+)+:\s+list items must be \w+, but found \w+, \w+"):
+        shared_informatieobject.validate()
+
+def test_validate_sequence_type_checking(shared_informatieobject):
+    """Test validation of sequence type checking."""
+
+    shared_informatieobject.naam = ["foo", "bar"]
+    with pytest.raises(ValidationError, match=r"\w+(\.\w+)+:\s+got type \w+, but field does not accept sequences"):
+        shared_informatieobject.validate()
 
 def test_validate_url(shared_informatieobject):
     """Test URL validation."""
