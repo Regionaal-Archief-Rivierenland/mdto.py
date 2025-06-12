6767be5ce9f43776681bb1bec61b2a06fcb7e67f
Rijnder Wever <rien334@gmail.com>
2025-06-12 14:58:47 +0200

    fix: add missing import

--- mdto/utilities.py
+++ mdto/utilities.py
@@ -3,7 +3,7 @@ import os
 import re
 from datetime import datetime
 from pathlib import Path
-from typing import List, TextIO
+from typing import List, TextIO, Type
 
 import lxml.etree as ET
 import pygfried
