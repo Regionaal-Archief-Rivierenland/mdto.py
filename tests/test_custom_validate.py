06e81d2ac86d6a98702df215f77c2092842dfd8b
Rijnder Wever <rien334@gmail.com>
2025-06-18 09:34:38 +0200

    chore: formatting

--- tests/test_custom_validate.py
+++ tests/test_custom_validate.py
@@ -19,18 +19,29 @@ def test_validate_recursive(shared_informatieobject):
     ):
         shared_informatieobject.validate()
 
+
 def test_validate_sequence_type_checking(shared_informatieobject):
     """Test validation of sequence type checking."""
 
-    shared_informatieobject.identificatie = [shared_informatieobject.identificatie, VerwijzingGegevens("foo")]
-    with pytest.raises(ValidationError, match=r"\w+(\.\w+)+:\s+list items must be \w+, but found \w+, \w+"):
+    shared_informatieobject.identificatie = [
+        shared_informatieobject.identificatie,
+        VerwijzingGegevens("foo"),
+    ]
+    with pytest.raises(
+        ValidationError,
+        match=r"\w+(\.\w+)+:\s+list items must be \w+, but found \w+, \w+",
+    ):
         shared_informatieobject.validate()
 
     # reset informatieobject back to normal
     shared_informatieobject.identificatie = shared_informatieobject.identificatie[0]
     shared_informatieobject.naam = ["foo", "bar"]
-    with pytest.raises(ValidationError, match=r"\w+(\.\w+)+:\s+got type \w+, but field does not accept sequences"):
+    with pytest.raises(
+        ValidationError,
+        match=r"\w+(\.\w+)+:\s+got type \w+, but field does not accept sequences",
+    ):
         shared_informatieobject.validate()
+
 
 def test_validate_url(shared_informatieobject):
     """Test URL validation."""
