8e6ba8c4d3fbdc547deee78c385b55985e6a7509
Rijnder Wever <rien334@gmail.com>
2025-11-12 19:03:46 +0100

    test: remove workaround
    NA used to have something wrong in their headers

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,9 +36,8 @@ def download_mdto_xsd(target_dir):
     response = requests.get(xsd_url)
     response.raise_for_status()  # raise error if download failed
 
-    # should be response.text and open(file, "w"), but NA is sending incorrect header information
-    with open(target_dir / xsd_filename, "wb") as f:
-        f.write(response.content)
+    with open(target_dir / xsd_filename, "w") as f:
+        f.write(response.text)
 
 
 @pytest.fixture
