d252bce133a6df69d8647681fbf017245cbd4aed
Rijnder Wever <rien334@gmail.com>
2025-12-23 12:36:18 +0100

    fix: catch more empty dataclass fields

--- mdto/classes.py
+++ mdto/classes.py
@@ -165,7 +165,7 @@ class Serializable:
         representation to `root_elem`."""
 
         # skip empty fields
-        if field_value is None:
+        if field_value is None or len(field_value) == 0:
             return
 
         # convert field_value to an iterable (if not already)
