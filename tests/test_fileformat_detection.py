b5e9e3234869a009bd1d38a660dc874e55ffba9c
Rijnder Wever <rien334@gmail.com>
2026-01-13 16:20:47 +0100

    tests: be more flexible about non-IANA types
    This helps if you use libmagic

--- tests/test_fileformat_detection.py
+++ tests/test_fileformat_detection.py
@@ -1,4 +1,5 @@
 import pytest
+import re
 
 from mdto.gegevensgroepen import *
 from mdto.helpers import pronominfo, mimetypeinfo
@@ -22,15 +23,14 @@ def test_mimetype(voorbeeld_pdf_file):
 
     assert expected == got
 
+
 # TODO: find out why siegfried won't recognize these
 # maybe this should be a little more relaxed; x-python is non-standard
 def test_mimetype_py():
     """Test mimetype detection for python files.
 
     These are esotoric/non-IANA recognized, and thus deserve a seperate test"""
-    expected = BegripGegevens(
-        "x-python", VerwijzingGegevens("IANA Media types"), "text/x-python"
-    )
+    expected = r"x-.*python"
     got = mimetypeinfo(__file__)
 
-    assert expected == got
+    assert re.search(expected, got.begripLabel)
